c6eaaa84409698e1b4f95858bb38035da1e6858a
Revaz-Goguadze <goguadze.rezi2022@gmail.com>
2023-09-01 19:14:44 +0400

    updated font script

--- install-fonts.sh
+++ install-fonts.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 sudo pacman -S --needed --noconfirm ttf-dejavu ttf-liberation noto-fonts ttf-roboto ttf-fira-code ttf-inconsolata ttf-opensans ttf-croscore ttf-droid ttf-ubuntu-font-family ttf-hack ttf-font-awesome
+
+yay -S ttf-ms-fonts noto-fonts-emoji-apple
