31713fdaf55a8c5ce34b93ac96e9464975e96d2e
Revaz-Goguadze <goguadze.rezi2022@gmail.com>
2023-09-01 17:32:32 +0400

    Initial commit

---
+++ arch-installation-packages.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo "Installation Will Be Started"
+echo "."
+sleep 0.2
+echo ".."
+sleep 0.2
+echo "..."
+sleep 0.2
+echo "...."
+sleep 0.2
+echo "Started ..."
+sleep 0.5
+
+# List of packages to install using pacman
+packages_to_install=(
+    git spectacle vim xarchiver p7zip zip bzip2 gzip tar unzip unrar zsh vi upower gparted tmux thunar ruby python-pip openvpn neofetch htop vlc dmenu rofi feh libinput intel-ucode mesa base-devel wget i3 i3-gaps ranger coreutils p7zip ripgrep tpm2-tools tpm2-tss pipewire-pulse ly nodejs npm typescript-language-server bash-language-server python-lsp-server yaml-language-server reflector firefox leafpad ruby-bundler
+)
+
+# Install packages using pacman
+sudo pacman -S --noconfirm --needed "${packages_to_install[@]}"
+
+# Install Yay
+git clone https://aur.archlinux.org/yay.git
+cd yay
+makepkg -si
+cd ..
+rm -rf yay
+
+echo "Packages and Yay installation completed."
+
+echo "----------------"
+echo "----------------"
+echo "----------------"
+
+read -p "Do you want add blackarch rep? (Y/N): " choice
+
+if [ "$choice" == "Y" ] || [ "$choice" == "y" ]; then
+    # Add BlackArch repository
+    echo "[blackarch]" | sudo tee -a /etc/pacman.conf
+    echo "Server = https://mirror.blackarch.org/\$repo/os/\$arch" | sudo tee -a /etc/pacman.conf
+
+    # Update package database
+    sudo pacman -Syy
+    echo "BlackArch repository added."
+else
+    echo "Only Updating ..."
+    sudo pacman -Syy
+fi
+
+-- configuring dots
+
+
+source_file="wallhaven-1p398w_1920x1080.png"
+destination_directory="$HOME/Downloads"
+
+# Check if the destination directory exists, and if not, create it
+if [ ! -d "$destination_directory" ]; then
+    mkdir -p "$destination_directory"
+fi
+
+# Copy the file to the destination directory
+cp "$source_file" "$destination_directory/"
+
+cp -r .config .font .icons /home/Documents
---
+++ test.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+#cp -r .config $home
+cp -r .font $home
+cp -r .icons $home
