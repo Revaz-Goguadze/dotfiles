ca70f621fbda292eb0b357b73d433063d03f8b38
Revaz-Goguadze <goguadze.rezi2022@gmail.com>
2023-09-01 19:11:04 +0400

    added installation fonts script

---
+++ install-fonts.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+sudo pacman -S --needed --noconfirm ttf-dejavu ttf-liberation noto-fonts ttf-roboto ttf-fira-code ttf-inconsolata ttf-opensans ttf-croscore ttf-droid ttf-ubuntu-font-family ttf-hack ttf-font-awesome
